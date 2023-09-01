e4b96edbeca23b3a70a7383ec45357cbdc89b5ab
Karol Sobolewski <karolsobolewski92@gmail.com>
2023-09-01 15:32:35 +0200

    Add Vercel Analytics

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -3,6 +3,7 @@ import GoogleAnalytics from "../GAnalytics";
 import CookieBanner from "../utils/CookieBanner";
 import Footer from "./Footer";
 import Header from "./Header";
+import { Analytics } from "@vercel/analytics/react";
 
 type LayoutProps = {
   children: ReactNode;
@@ -12,7 +13,7 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <div className="flex flex-col min-h-screen">
       <GoogleAnalytics GA_MEASUREMENT_ID="G-0F23B5CF3J" />
-
+      <Analytics />
       <Header />
       <main className="flex-grow w-full mx-auto">{children}</main>
       <CookieBanner />
